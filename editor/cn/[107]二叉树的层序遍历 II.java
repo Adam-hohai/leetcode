6b88ff0cdce02bec14eae65ab4648f07f4022890
cenhelm <hohai.shenghao@gmail.com>
2022-04-06 11:18:21 +0800

    22.4.6

---
+++ editor/cn/[107]二叉树的层序遍历 II.java
@@ -0,0 +1,76 @@
+//给你二叉树的根节点 root ，返回其节点值 自底向上的层序遍历 。 （即按从叶子节点所在层到根节点所在的层，逐层从左向右遍历） 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：root = [3,9,20,null,null,15,7]
+//输出：[[15,7],[9,20],[3]]
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：root = [1]
+//输出：[[1]]
+// 
+//
+// 示例 3： 
+//
+// 
+//输入：root = []
+//输出：[]
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 树中节点数目在范围 [0, 2000] 内 
+// -1000 <= Node.val <= 1000 
+// 
+// Related Topics 树 广度优先搜索 二叉树 👍 548 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+
+import java.util.*;
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<List<Integer>> levelOrderBottom(TreeNode root) {
+        if (root == null) return Collections.emptyList();
+        Deque<TreeNode> deque = new ArrayDeque<TreeNode>();
+        deque.offer(root);
+        List<List<Integer>> ans = new ArrayList<>();
+        while (!deque.isEmpty()){
+            List<Integer> t = new ArrayList<>();
+            for (int i = 0, n = deque.size(); i < n; ++i){
+                TreeNode node = deque.pollFirst();
+                t.add(node.val);
+                if (node.left != null) deque.offer(node.left);
+                if (node.right != null) deque.offer(node.right);
+            }
+            ans.add(t);
+        }
+        Collections.reverse(ans);
+        return ans;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
