e7829f04a16eb90f30c4ecfe882c081d90193fb1
cenhelm <hohai.shenghao@gmail.com>
2022-04-07 16:04:29 +0800

    22.4.7

---
+++ editor/cn/[199]二叉树的右视图.java
@@ -0,0 +1,76 @@
+//给定一个二叉树的 根节点 root，想象自己站在它的右侧，按照从顶部到底部的顺序，返回从右侧所能看到的节点值。 
+//
+// 
+//
+// 示例 1: 
+//
+// 
+//
+// 
+//输入: [1,2,3,null,5,null,4]
+//输出: [1,3,4]
+// 
+//
+// 示例 2: 
+//
+// 
+//输入: [1,null,3]
+//输出: [1,3]
+// 
+//
+// 示例 3: 
+//
+// 
+//输入: []
+//输出: []
+// 
+//
+// 
+//
+// 提示: 
+//
+// 
+// 二叉树的节点个数的范围是 [0,100] 
+// -100 <= Node.val <= 100 
+// 
+// Related Topics 树 深度优先搜索 广度优先搜索 二叉树 👍 654 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+
+import java.util.*;
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<Integer> rightSideView(TreeNode root) {
+        if (root == null) return Collections.emptyList();
+        List<Integer> ans = new ArrayList<>();
+        Deque<TreeNode> deque = new ArrayDeque<TreeNode>();
+        deque.offer(root);
+        while (!deque.isEmpty()){
+            int len = deque.size();
+            for (int i = 0; i < len; ++i){
+                TreeNode node = deque.pollLast();
+                if (i == 0) ans.add(node.val);
+                if (node.right != null) deque.offerFirst(node.right);
+                if (node.left != null) deque.offerFirst(node.left);
+            }
+        }
+        return ans;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
