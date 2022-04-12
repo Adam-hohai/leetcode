319c949e075f0c304d8d1580f885fa778c6b234a
Adam-hohai <2272290944@qq.com>
2022-04-12 11:30:29 +0800

    22.4.12

---
+++ editor/cn/[113]路径总和 II.java
@@ -0,0 +1,93 @@
+//给你二叉树的根节点 root 和一个整数目标和 targetSum ，找出所有 从根节点到叶子节点 路径总和等于给定目标和的路径。 
+//
+// 叶子节点 是指没有子节点的节点。 
+//
+// 
+// 
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：root = [5,4,8,11,null,13,4,7,2,null,null,5,1], targetSum = 22
+//输出：[[5,4,11,2],[5,8,4,5]]
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：root = [1,2,3], targetSum = 5
+//输出：[]
+// 
+//
+// 示例 3： 
+//
+// 
+//输入：root = [1,2], targetSum = 0
+//输出：[]
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 树中节点总数在范围 [0, 5000] 内 
+// -1000 <= Node.val <= 1000 
+// -1000 <= targetSum <= 1000 
+// 
+// 
+// 
+// Related Topics 树 深度优先搜索 回溯 二叉树 👍 721 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode() {}
+ * TreeNode(int val) { this.val = val; }
+ * TreeNode(int val, TreeNode left, TreeNode right) {
+ * this.val = val;
+ * this.left = left;
+ * this.right = right;
+ * }
+ * }
+ */
+class Solution {
+    private List<List<Integer>> ans;
+    private List<Integer> path;
+
+    public List<List<Integer>> pathSum(TreeNode root, int targetSum) {
+        ans = new ArrayList<>();
+        path = new ArrayList<>();
+        dfs(root, targetSum);
+        return ans;
+    }
+
+    private void dfs(TreeNode root, int targetSum) {
+        if (root == null) return;
+        path.add(root.val);
+//        int sum = path.stream().mapToInt(Integer::intValue).sum();
+//        System.out.println(sum);
+//        System.out.println(path);
+//        if (root.left == null && root.right == null) System.out.println("True");
+        if (root.val == targetSum && root.left == null && root.right == null) {
+//            System.out.println(ans);
+            ans.add(new ArrayList<>(path));//这步直接add path就有问题，好迷
+//            System.out.println(ans);
+        }
+        dfs(root.left, targetSum - root.val);
+        dfs(root.right, targetSum - root.val);
+        path.remove(path.size() - 1);
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
