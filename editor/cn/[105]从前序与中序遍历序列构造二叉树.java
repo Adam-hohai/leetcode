e9028bd3b2563f6a7783b07cb42996573bd757a3
Adam-hohai <2272290944@qq.com>
2022-04-13 13:55:41 +0800

    22.4.13

---
+++ editor/cn/[105]从前序与中序遍历序列构造二叉树.java
@@ -0,0 +1,77 @@
+//给定两个整数数组 preorder 和 inorder ，其中 preorder 是二叉树的先序遍历， inorder 是同一棵树的中序遍历，请构造二叉树并
+//返回其根节点。 
+//
+// 
+//
+// 示例 1: 
+//
+// 
+//输入: preorder = [3,9,20,15,7], inorder = [9,3,15,20,7]
+//输出: [3,9,20,null,null,15,7]
+// 
+//
+// 示例 2: 
+//
+// 
+//输入: preorder = [-1], inorder = [-1]
+//输出: [-1]
+// 
+//
+// 
+//
+// 提示: 
+//
+// 
+// 1 <= preorder.length <= 3000 
+// inorder.length == preorder.length 
+// -3000 <= preorder[i], inorder[i] <= 3000 
+// preorder 和 inorder 均 无重复 元素 
+// inorder 均出现在 preorder 
+// preorder 保证 为二叉树的前序遍历序列 
+// inorder 保证 为二叉树的中序遍历序列 
+// 
+// Related Topics 树 数组 哈希表 分治 二叉树 👍 1538 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode() {}
+ * TreeNode(int val) { this.val = val; }
+ * TreeNode(int val, TreeNode left, TreeNode right) {
+ * this.val = val;
+ * this.left = left;
+ * this.right = right;
+ * }
+ * }
+ */
+class Solution {
+    private Map<Integer, Integer> inorder_map = new HashMap<>();
+
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+        int n = inorder.length;
+        for (int i = 0; i < n; ++i) {
+            inorder_map.put(inorder[i], i);
+        }
+        return build(preorder, inorder, 0, n - 1, 0, n - 1);
+    }
+
+    private TreeNode build(int[] preorder, int[] inorder, int p1, int p2, int i1, int i2) {
+        if (p1 > p2 || i1 > i2) return null;
+        int rootVal = preorder[p1];
+        int pos = inorder_map.get(rootVal);
+        TreeNode node = new TreeNode(rootVal);
+        node.left = pos == i1 ? null : build(preorder, inorder, p1 + 1, p1 + pos - i1, i1, pos - 1);
+        node.right = pos == i2 ? null : build(preorder, inorder, p1 + pos - i1 + 1, p2, pos + 1, i2);
+        return node;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
