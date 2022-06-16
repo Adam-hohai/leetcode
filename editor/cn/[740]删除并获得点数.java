6e58da37d71d74f31216983dfe0266e19ea2a03b
Cenhelm <oncwnuJgqX-hn-IRVYHyU5zvOan4@git.weixin.qq.com>
2022-06-16 15:51:43 +0800

    22.6.16

---
+++ editor/cn/[740]删除并获得点数.java
@@ -0,0 +1,67 @@
+//给你一个整数数组 nums ，你可以对它进行一些操作。 
+//
+// 每次操作中，选择任意一个 nums[i] ，删除它并获得 nums[i] 的点数。之后，你必须删除 所有 等于 nums[i] - 1 和 nums[i]
+// + 1 的元素。 
+//
+// 开始你拥有 0 个点数。返回你能通过这些操作获得的最大点数。 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：nums = [3,4,2]
+//输出：6
+//解释：
+//删除 4 获得 4 个点数，因此 3 也被删除。
+//之后，删除 2 获得 2 个点数。总共获得 6 个点数。
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：nums = [2,2,3,3,3,4]
+//输出：9
+//解释：
+//删除 3 获得 3 个点数，接着要删除两个 2 和 4 。
+//之后，再次删除 3 获得 3 个点数，再次删除 3 获得 3 个点数。
+//总共获得 9 个点数。
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 1 <= nums.length <= 2 * 10⁴ 
+// 1 <= nums[i] <= 10⁴ 
+// 
+// Related Topics 数组 哈希表 动态规划 👍 638 👎 0
+
+
+import java.util.Map;
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public int deleteAndEarn(int[] nums) {
+        if (nums.length == 0){
+            return 0;
+        }
+        int[] sums = new int[10010];
+        int[] select = new int[10010];
+        int[] nonSelect = new int[10010];
+
+        int maxV = 0;
+        for (int x : nums){
+            sums[x] += x;
+            maxV = Math.max(maxV, x);
+        }
+
+        for (int i = 1; i <= maxV; ++i){
+            select[i] = nonSelect[i - 1] + sums[i];
+            nonSelect[i] = Math.max(select[i - 1], nonSelect[i - 1]);
+        }
+        return Math.max(select[maxV], nonSelect[maxV]);
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
